d2916a3f39e3ed1ffb3923e27eb1c1dbf98a03d8
Phillip Booker <phillippbooker@gmail.com>
2019-05-12 19:53:56 -0700

    Adds do-what-it-says command. Handles artist/song not found cases.

--- liri.js
+++ liri.js
@@ -14,6 +14,9 @@ var axios = require("axios");
 // Include moment package
 var moment = require('moment');
 
+// Include fs package (internal to node)
+var fs = require("fs");
+
 // Command line arguments
 var args = process.argv;
 
@@ -28,20 +31,74 @@ console.log("**********************");
 
 switch(command){
     case 'concert-this':
-        //Concert commands
-        if(title !== undefined){
-            var query = title.split(" ").join("+").split('.').join("");
-            console.log("Query: " + query);
-            axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp")
-            .then(function(response){
-                // console.log(response);
-                // var concerts = JSON.parse(response);
-                
-                // console.log(response);
-    
-                var concerts = response.data;
-                console.log("Type of concerts: " + typeof concerts);
-                if(typeof concerts === "object"){
+        concertThis(title);
+        break;
+    case 'spotify-this-song':
+        spotifyThis(title);
+        break;
+    case 'movie-this':
+        movieThis(title);
+        break;
+    case 'do-what-it-says':
+        // read file
+        fs.readFile('./random.txt', 'utf8', function(error, data){
+            if(error){
+                return console.log(error);
+            }
+            var textCommand = data.split(",");
+            
+            var fCommand = textCommand[0];
+            var fTitle;
+            
+            if(textCommand[1] !== undefined){
+                // Removes double quotes from title in text file
+                fTitle = textCommand[1].replace(/["]+/g, '');
+            }
+
+            console.log("Text Command: " + fCommand);
+            console.log("Text Title: " + fTitle);
+
+            switch(fCommand){
+                case 'concert-this':
+                    concertThis(fTitle);
+                    break;
+                case 'spotify-this-song':
+                    spotifyThis(fTitle);
+                    break;
+                case 'movie-this':
+                    movieThis(fTitle);
+                    break;
+                case 'do-what-it-says':
+                    console.log("We don't want any loops now...");
+                    break;
+                default:
+                    console.log("Unknown command: " + fCommand);
+            }
+        });
+        break;
+    case 'help':
+        console.log("COMMANDS:");
+        console.log("concert-this '<band>': Shows concert info for a given band");
+        console.log("spotify-this-song '<song-name>': Shows song information from Spotify");
+        console.log("movie-this '<movie-title>': Shows movie information for the given title");
+        console.log("do-what-it-says: Executes command in the 'random.txt' file (format: <command>,\"<title>\")");
+        break;
+    default:
+        console.log("Unknown command (type 'help' for list of commands)");
+}
+
+function concertThis(artist){
+    if(artist !== undefined){
+        var query = artist.split(" ").join("+").split('.').join("");
+        console.log("Query: " + query);
+        axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp")
+        .then(function(response){
+            // console.log(response);
+            var concerts = response.data;
+
+            console.log("Type of concerts: " + typeof concerts);
+            if(typeof concerts === "object"){
+                if(concerts.length > 0){
                     console.log("Concerts ==============");
                     concerts.forEach(function(concert, i){
                         var venue = concert.venue;
@@ -52,30 +109,35 @@ switch(command){
                         console.log("+++++");
                     });
                 } else{
-                    console.log("No concerts found...");
+                    console.log("No concerts found.");
                 }
                 
-                console.log("**********************");
-            });
-        } else {
-            console.log("Please enter an artist");
+            } else{
+                console.log("Artist not found...");
+            }
+            
+            console.log("**********************");
+        });
+    } else {
+        console.log("Please enter an artist");
+    }
+}
+
+function spotifyThis(title){
+    if(title === undefined){
+        title = 'the sign ace of base';
+    }
+    spotify.search({ type: 'track', query: title }, function(error, data){
+        if(error){
+            return console.log("Error occurred: " + error);
         }
         
-        break;
-    case 'spotify-this-song':
-        if(title === undefined){
-            title = 'the sign ace of base';
-        }
-        spotify.search({ type: 'track', query: title }, function(error, data){
-            if(error){
-                return console.log("Error occurred: " + error);
-            }
-            // If there are no results? Is an error returned?
+        // console.log(data);
+        if(data.tracks.items.length > 0){
+            // Uses first result
             var track = data.tracks.items[0];
-            // console.log(data.tracks.items[0]);
-            // console.log(JSON.stringify(data.tracks.items[0], null, 2))
 
-            //Track Artists
+            // Track Artists
             var artistsObj = track.artists;
             var artists = [];
             artistsObj.forEach(function(artist){
@@ -96,60 +158,48 @@ switch(command){
             console.log("Preview Link: " + preview);
             console.log("Album: " + album);
             console.log("**********************");
-        });
-        break;
-    case 'movie-this':
-        if(title === undefined){
-            title = 'Mr. Nobody';
+        } else{
+            console.log("Song not found");
         }
-        var query = title.split(" ").join("+").split('.').join("");
-        axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").
-        then(function(response) {
-            // console.log(response.data);
-            // console.log(response.data.Error);
-            if(response.data.Error){
-                return console.log(response.data.Error);
-            } else{
-                // console.log(response);
-                console.log("Title: " + response.data.Title);
-                console.log("Year: " + response.data.Year);
-                console.log("IMDB Rating: " + response.data.imdbRating);
-                //Ratings
-                var ratings = response.data.Ratings
-                var tomatoRating = "N/A";
-                if(typeof ratings === "object"){
-                    for(key in ratings){
-                        var source = ratings[key].Source;
-                        if(source === "Rotten Tomatoes"){
-                            tomatoRating = ratings[key].Value;
-                        }
-                    }
-                }
-                
-                console.log("Rotten Tomatoes Rating: " + tomatoRating);
-                
-                console.log("Country: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
-            }
-            console.log("**********************");
-        });
-        break;
-    case 'do-what-it-says':
-        // read file
-        // parse command/title
-        // switch
-        // call necessary function
-        break;
-    case 'help':
-        console.log("COMMANDS:")
-        console.log("concert-this '<band>': Shows concert info for a given band")
-        console.log("spotify-this-song '<song-name>': Shows song information from Spotify")
-        console.log("movie-this '<movie-title>': Shows movie information for the given title")
-        console.log("do-what-it-says: Executes commands in the 'random.txt' file")
-        break;
-    default:
-        console.log("Unknown command (type 'help' for list of commands)");
+        
+    });
 }
 
+function movieThis(title){
+    if(title === undefined){
+        title = 'Mr. Nobody';
+    }
+    var query = title.split(" ").join("+").split('.').join("");
+    axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").
+    then(function(response) {
+
+        if(response.data.Error){
+            return console.log(response.data.Error);
+        } else{
+            // console.log(response);
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+
+            //Ratings
+            var ratings = response.data.Ratings
+            var tomatoRating = "N/A";
+            if(typeof ratings === "object"){
+                for(key in ratings){
+                    var source = ratings[key].Source;
+                    if(source === "Rotten Tomatoes"){
+                        tomatoRating = ratings[key].Value;
+                    }
+                }
+            }
+            
+            console.log("Rotten Tomatoes Rating: " + tomatoRating);
+            
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }
+        console.log("**********************");
+    });
+}
